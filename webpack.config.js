c6b290f043c1c722ff55f3b3d1679a9a5d1bf1cc
99slayer <21979862+99slayer@users.noreply.github.com>
2023-08-16 22:26:27 -0600

    - Installed react and babel.
    - Changed npm scripts.
    
    - Setup webpack-dev-server and babel loader in webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, './dist')
+    }
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, './src/index.html')
@@ -11,13 +16,28 @@ module.exports = {
   ],
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist/'),
+    clean: true
   },
   module: {
     rules: [
       {
+        test: /.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
       }
     ]
   }
