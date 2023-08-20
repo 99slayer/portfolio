1d40f50510fe27f82a0ae610ed7c3059926fb640
99slayer <21979862+99slayer@users.noreply.github.com>
2023-08-19 18:59:47 -0600

    - Created the show and hide functions to handle the visibleArr state.

--- src/components/Display.js
+++ src/components/Display.js
@@ -4,15 +4,24 @@ import '../css/Display.css';
 import fileIcon1 from '../assets/file-icon-1.png';
 import folderIcon2 from '../assets/folder-icon-2.png';
 import { projectDetails } from '../project-details';
+import { Taskbar } from './Taskbar';
 import { Projects } from './Projects';
 import { Contact } from './About';
 import { File } from './File';
 
 export function Display() {
   const [openArr, setOpenArr] = useState([]);
+  const [visibleArr, setVisibleArr] = useState([]);
+
+  // this is for keeping track of all the open windows z-index rankings.
+  const [activeArr, setActiveArr] = useState([]);
 
   const openWindow = (x) => {
     // x needs to match the project-details.js filename for each project file.
+    if (openArr.includes(x)) {
+      return;
+    }
+
     setOpenArr(openArr.concat(x));
   }
 
@@ -26,12 +35,31 @@ export function Display() {
     };
   };
 
+  const show = (x) => {
+    if (visibleArr.includes(x)) {
+      return;
+    }
+
+    setVisibleArr(visibleArr.concat(x));
+  };
+
+  const hide = (x) => {
+    const copy = [...visibleArr];
+    const index = copy.indexOf(x);
+
+    if (index >= 0) {
+      copy.splice(index, 1);
+      setVisibleArr(copy);
+    };
+  };
+
   return (
     <div id='display'>
       <div id='display-grid'>
         <div className='display-cell'>
           <div id='projects-icon' className='display-icon' onClick={() => {
-            openWindow('project folder');
+            openWindow('My Projects');
+            show('My Projects');
           }}>
             <img src={folderIcon2} />
             <p>My Projects</p>
@@ -39,16 +67,18 @@ export function Display() {
         </div>
         <div className='display-cell'>
           <div id='about-me-icon' className='display-icon' onClick={() => {
-            openWindow('about me');
+            openWindow('About Me');
+            show('About Me');
           }}>
             <img src={fileIcon1} />
             <p>About Me</p>
           </div>
         </div>
       </div>
-      <File openArr={openArr} closeWindow={closeWindow} details={projectDetails.photoTaggingApp} />
-      <Projects openArr={openArr} closeWindow={closeWindow} />
-      <Contact openArr={openArr} closeWindow={closeWindow} />
+      <Taskbar openArr={openArr} show={show} />
+      <File openArr={openArr} visibleArr={visibleArr} hide={hide} closeWindow={closeWindow} details={projectDetails.photoTaggingApp} />
+      <Projects openArr={openArr} visibleArr={visibleArr} hide={hide} closeWindow={closeWindow} />
+      <Contact openArr={openArr} visibleArr={visibleArr} hide={hide} closeWindow={closeWindow} />
     </div>
   )
 }
