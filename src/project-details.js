a727b7ffe07e85a997fe93d427ec233726516f74
99slayer <21979862+99slayer@users.noreply.github.com>
2023-08-21 16:32:28 -0600

    - Added a few more placeholder objects.

--- src/project-details.js
+++ src/project-details.js
@@ -1,6 +1,33 @@
 export const projectDetails = {
-  photoTaggingApp: {
-    name: '',
+  project1: {
+    name: 'project1',
+    image: '',
+    description: '',
+    links: {
+      github: '',
+      live: ''
+    }
+  },
+  project2: {
+    name: 'project2',
+    image: '',
+    description: '',
+    links: {
+      github: '',
+      live: ''
+    }
+  },
+  project3: {
+    name: 'project3',
+    image: '',
+    description: '',
+    links: {
+      github: '',
+      live: ''
+    }
+  },
+  project4: {
+    name: 'project4',
     image: '',
     description: '',
     links: {
