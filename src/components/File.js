1a01308993e38ce130e3da2d9fe6fce97e574369
99slayer <21979862+99slayer@users.noreply.github.com>
2023-08-21 16:38:42 -0600

    - Sorted out visibleArr and openArr effects.

--- src/components/File.js
+++ src/components/File.js
@@ -6,27 +6,41 @@ import closeIcon from '../assets/close-icon.png';
 import fileIcon3 from '../assets/file-icon-3.png';
 
 export function File(props) {
-  const { openArr, details } = props;
+  const { openArr, visibleArr, closeWindow, hide, getWindowZIndex, setToActive, details } = props;
 
-  const [open, setOpen] = useState(false);
+  const [visible, setVisible] = useState(false);
 
   useEffect(() => {
-    if (openArr.includes(details.name)) {
-      setOpen(true);
-    }
+    if (!openArr.includes(details.name)) {
+      hide(details.name);
+    };
   }, [openArr])
 
+  useEffect(() => {
+    if (visibleArr.includes(details.name)) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    };
+  }, [visibleArr])
+
   return (
-    <section className={`${open ? 'open' : ''} file-template`}>
+    <section className={`${visible ? 'visible' : ''} file-template`} style={{ zIndex: getWindowZIndex(details.name) }} onClick={() => {
+      setToActive(details.name);
+    }}>
       <header className='file-header'>
         <div className='file-heading'>
           <img className='heading-icon' src={fileIcon3} />
           <h3 className='heading-text'>FILE NAME</h3>
         </div>
         <div className='file-window-btns'>
-          <button className='file-window-btn'><img src={minimizeIcon} /></button>
+          <button className='file-window-btn'><img src={minimizeIcon} onClick={() => {
+            hide(details.name);
+          }} /></button>
           <button className='file-window-btn'><img src={maximizeIcon} /></button>
-          <button className='file-window-btn'><img src={closeIcon} /></button>
+          <button className='file-window-btn' onClick={() => {
+            closeWindow(details.name);
+          }}><img src={closeIcon} /></button>
         </div>
       </header>
       <div className='file-tabs'>
