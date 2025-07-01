4f71cf817a8985137930889b862f448a67b51735
99slayer <21979862+99slayer@users.noreply.github.com>
2025-07-01 17:38:36 -0600

    feat: Add copy button to email start menu tab

--- src/components/StartMenu.tsx
+++ src/components/StartMenu.tsx
@@ -49,6 +49,26 @@ function StartMenu() {
 						</div>
 						<p>{social.name}</p>
 					</a>
+					{social.name === 'EMAIL' ?
+						<div
+							className='size-[43px] ml-[4px] p-[4px] flex justify-center items-center border-[2px] border-black bg-white'
+							title='copy email'
+							onClick={() => {
+								navigator.clipboard.writeText(import.meta.env.VITE_EMAIL);
+							}}
+						>
+							<svg
+								id="Copy"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+							>
+								<polygon points="16 20 16 22 15 22 15 23 3 23 3 22 2 22 2 6 3 6 3 5 6 5 6 20 16 20" />
+								<path d="m16,7V1h-8v1h-1v16h1v1h13v-1h1V7h-6Zm4,10h-11V3h5v6h6v8Z" />
+								<polygon points="22 5 22 6 17 6 17 1 18 1 18 2 19 2 19 3 20 3 20 4 21 4 21 5 22 5" />
+							</svg>
+						</div> :
+						<></>
+					}
 				</button>
 			);
 		}
